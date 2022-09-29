81dae09fe0b526578c64b7245976288129bb3a63
Daniel Kim <danielkim2711@gmail.com>
2022-09-29 22:34:49 +1300

    Create pet controller

---
+++ backend/controllers/petController.js
@@ -0,0 +1,127 @@
+const asyncHandler = require('express-async-handler');
+
+const Pet = require('../models/petModel');
+
+// @Desc    Get all pets of user
+// @Route   GET /api/pets
+// @Access  Private
+const getPets = asyncHandler(async (req, res) => {
+  const pets = await Pet.find({ user: req.user._id });
+
+  if (!req.user) {
+    res.status(400);
+    throw new Error('User not found');
+  }
+
+  res.status(200).json(pets);
+});
+
+// @Desc    Get a pet
+// @Route   GET /api/pets/:id
+// @Access  Private
+const getPet = asyncHandler(async (req, res) => {
+  const pet = await Pet.findById(req.params.id);
+
+  if (!req.user) {
+    res.status(400);
+    throw new Error('User not found');
+  }
+
+  if (!pet) {
+    res.status(400);
+    throw new Error('Pet not found');
+  }
+
+  // Check the logged in user matches with the pet user
+  if (pet.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error('Invalid Request. Unauthorised');
+  }
+
+  res.status(200).json(pet);
+});
+
+// @Desc    Create pet
+// @Route   POST /api/pets
+// @Access  Private
+const createPet = asyncHandler(async (req, res) => {
+  const { name, age, type } = req.body;
+
+  if (!name || !age || !type) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  const pet = await Pet.create({
+    user: req.user._id,
+    name,
+    age,
+    type,
+  });
+
+  res.status(201).json(pet);
+});
+
+// @Desc    Update pet
+// @Route   PUT /api/pets/:id
+// @Access  Private
+const updatePet = asyncHandler(async (req, res) => {
+  const pet = await Pet.findById(req.params.id);
+
+  if (!req.user) {
+    res.status(400);
+    throw new Error('User not found');
+  }
+
+  if (!pet) {
+    res.status(400);
+    throw new Error('Pet not found');
+  }
+
+  // Check the logged in user matches with the pet user
+  if (pet.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error('Invalid Request. Unauthorised');
+  }
+
+  const updatedPet = await Pet.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedPet);
+});
+
+// @Desc    Delete pet
+// @Route   DELETE /api/pets/:id
+// @Access  Private
+const deletePet = asyncHandler(async (req, res) => {
+  const pet = await Pet.findById(req.params.id);
+
+  if (!req.user) {
+    res.status(400);
+    throw new Error('User not found');
+  }
+
+  if (!pet) {
+    res.status(400);
+    throw new Error('Pet not found');
+  }
+
+  // Check the logged in user matches with the pet user
+  if (pet.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error('Invalid Request. Unauthorised');
+  }
+
+  await pet.remove();
+
+  res.status(200).json({ _id: req.params.id, success: true });
+});
+
+module.exports = {
+  getPets,
+  getPet,
+  createPet,
+  updatePet,
+  deletePet,
+};
--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getPets,
+  getPet,
+  createPet,
+  updatePet,
+  deletePet,
+} = require('../controllers/petController');
 const protect = require('../middleware/authMiddleware');
 
-router.route('/').get(getPets).post(protect, createPet);
+router.route('/').get(protect, getPets).post(protect, createPet);
 router
   .route('/:id')
-  .get(getPet)
+  .get(protect, getPet)
   .put(protect, updatePet)
   .delete(protect, deletePet);
 
