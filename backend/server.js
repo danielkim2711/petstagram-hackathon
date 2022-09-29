184c6347b100c2f7c693657f6e0089eb76633d5d
Daniel Kim <danielkim2711@gmail.com>
2022-09-29 16:39:58 +1300

    Connect back end to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
 const port = process.env.PORT || 8000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
