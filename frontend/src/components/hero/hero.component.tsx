e204ac31659f6b7dba45369c26146e72a4fcf48c
Daniel Kim <danielkim2711@gmail.com>
2022-09-30 10:06:06 +1300

    Create hero component

--- frontend/src/components/hero/hero.component.tsx
+++ frontend/src/components/hero/hero.component.tsx
@@ -2,8 +2,23 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <h1 className='text-4xl text-red-500'>Hello Team</h1>
+    <div className='hero min-h-screen'>
+      <div className='hero-overlay bg-opacity-60'></div>
+      <div className='hero-content text-center text-neutral-content'>
+        <div className='max-w-md'>
+          <h1 className='mb-5 text-5xl font-bold'>
+            Loving Pet Sitting &amp; Dog Walking
+          </h1>
+          <p className='mb-5'>
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className=' bg-[#e69765] text-white px-4 py-3 rounded-lg uppercase font-semibold hover:scale-105 transition-all duration-200'>
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
