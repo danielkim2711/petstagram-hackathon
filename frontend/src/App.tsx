91daf05d38b3be22eb895040d86f1f45dd076be9
Daniel Kim <danielkim2711@gmail.com>
2022-09-29 13:17:16 +1300

    Set up the front end

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h1 className='text-4xl text-red-500'>Hello Team</h1>
+    </div>
+  );
+}
+
+export default App;
