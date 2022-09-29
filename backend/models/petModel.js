3792d9b5ddc468703eaa2c5dec7da356d349f79c
Daniel Kim <danielkim2711@gmail.com>
2022-09-29 21:18:34 +1300

    Create pet schema

---
+++ backend/models/petModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const petSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter your pet's name"],
+    },
+    age: {
+      type: Number,
+      required: [true, "Please enter your pet's age"],
+    },
+    type: {
+      type: String,
+      required: [true, "Please enter your pet's type"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Pet', petSchema);
