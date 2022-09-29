f97e56de6460c1b9e2f6655ed5d1fc6bc38503b6
Daniel Kim <danielkim2711@gmail.com>
2022-09-30 00:01:28 +1300

    Create log in component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+import LogIn from './routes/log-in/log-in.component';
 import SignUp from './routes/sign-up/sign-up.component';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='log-in' element={<LogIn />} />
         <Route path='sign-up' element={<SignUp />} />
       </Route>
     </Routes>
--- frontend/src/routes/log-in/log-in.component.tsx
+++ frontend/src/routes/log-in/log-in.component.tsx
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom';
 type Props = {};
 
 const defaultFormFields = {
-  name: '',
   email: '',
   password: '',
-  confirmPassword: '',
 };
 
-const SignUp = (props: Props) => {
+const LogIn = (props: Props) => {
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const { name, email, password, confirmPassword } = formFields;
+  const { email, password } = formFields;
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -27,14 +25,7 @@ const SignUp = (props: Props) => {
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
-      alert('Passwords do not match');
-      resetFormFields();
-      return;
-    }
-
     const userData = {
-      name,
       email,
       password,
     };
@@ -47,29 +38,16 @@ const SignUp = (props: Props) => {
     <div className='hero min-h-screen bg-base-200'>
       <div className='hero-content flex-col'>
         <div className='text-center'>
-          <h1 className='text-5xl font-bold'>Sign up</h1>
+          <h1 className='text-5xl font-bold'>Log In</h1>
           <p className='text-lg py-6 tracking-wider'>
-            Already a member?{' '}
-            <Link to='/log-in'>
-              <span className='text-[#008294]'>Log in</span>
+            Not a member yet?{' '}
+            <Link to='/sign-up'>
+              <span className='text-[#008294]'>Sign up</span>
             </Link>
           </p>
         </div>
         <div className='card flex-shrink-0 w-screen max-w-sm shadow-2xl bg-base-100'>
           <form className='card-body' onSubmit={handleSubmit}>
-            <div className='form-control'>
-              <label className='label'>
-                <span className='label-text'>Name</span>
-              </label>
-              <input
-                className='input input-bordered'
-                type='text'
-                name='name'
-                value={name}
-                onChange={handleChange}
-                required
-              />
-            </div>
             <div className='form-control'>
               <label className='label'>
                 <span className='label-text'>Email</span>
@@ -95,26 +73,18 @@ const SignUp = (props: Props) => {
                 onChange={handleChange}
                 required
               />
-            </div>
-            <div className='form-control'>
-              <label className='label'>
-                <span className='label-text'>Confirm Password</span>
-              </label>
-              <input
-                className='input input-bordered'
-                type='password'
-                name='confirmPassword'
-                value={confirmPassword}
-                onChange={handleChange}
-                required
-              />
+              {/* <label className='label'>
+                  <a href='#' className='label-text-alt link link-hover'>
+                    Forgot password?
+                  </a>
+                </label> */}
             </div>
             <div className='form-control mt-6'>
               <button
                 className='btn btn-primary hover:bg-black hover:text-white transition-all duration-300'
                 type='submit'
               >
-                Sign Up
+                Log In
               </button>
             </div>
           </form>
@@ -124,4 +94,4 @@ const SignUp = (props: Props) => {
   );
 };
 
-export default SignUp;
+export default LogIn;
