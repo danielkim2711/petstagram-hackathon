af295a0082a05277a459528e449c551648d794ed
Daniel Kim <danielkim2711@gmail.com>
2022-10-05 19:01:50 +1300

    Fix typo on footer component

--- frontend/src/components/footer/footer.component.tsx
+++ frontend/src/components/footer/footer.component.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className='footer footer-center p-8 bg-[#212121] text-white'>
       <div>
-        <p>Copyright © 2022 - All right reserved by Quack Quack hackathon</p>
+        <p>Copyright © 2022 - All rights reserved by Quack Quack hackathon</p>
       </div>
     </footer>
   );
